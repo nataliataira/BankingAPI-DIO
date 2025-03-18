8a7cfae62474c4da87a2f921da9585401aa73656
Natalia Taira <nataliagtaira@gmail.com>
2025-03-18 16:21:36 -0300

    feat: add UserController

---
+++ src/main/java/dio/me/bank_api/controller/UserController.java
@@ -0,0 +1,36 @@
+package dio.me.bank_api.controller;
+
+import dio.me.bank_api.domain.model.User;
+import dio.me.bank_api.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        var user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user) {
+        var newUser = userService.create(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(newUser);
+    }
+}
