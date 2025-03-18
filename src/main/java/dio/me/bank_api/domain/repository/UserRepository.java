a984857f6c47882b4beffbfd6f685209d9d4b424
Natalia Taira <nataliagtaira@gmail.com>
2025-03-18 16:22:43 -0300

    feat: add UserRepository

--- src/main/java/dio/me/bank_api/domain/repository/UserRepository.java
+++ src/main/java/dio/me/bank_api/domain/repository/UserRepository.java
@@ -4,4 +4,6 @@ import dio.me.bank_api.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository  extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
 }
